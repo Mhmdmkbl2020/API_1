fad938315f4d0cb1f97febc09c3002393554aaa4
Mhmdmkbl2020 <133782007+Mhmdmkbl2020@users.noreply.github.com>
2025-04-27 23:01:31 +0300

    Create ProductDto.cs

---
+++ API/Models/ProductDto.cs
@@ -0,0 +1,10 @@
+// API/Models/ProductDto.cs
+namespace API.Models
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long LastModified { get; set; }
+    }
+}
