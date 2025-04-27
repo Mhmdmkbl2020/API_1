aced57ed59c474b76877f4f705b990307e91744e
Mhmdmkbl2020 <133782007+Mhmdmkbl2020@users.noreply.github.com>
2025-04-27 23:02:16 +0300

    Create AppDbContext.cs

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+// API/Data/AppDbContext.cs
+using Microsoft.EntityFrameworkCore;
+using API.Models;
+
+namespace API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
