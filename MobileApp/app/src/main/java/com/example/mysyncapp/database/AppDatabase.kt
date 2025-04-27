eb586eddb515276a99f777c518122f57626ed378
Mhmdmkbl2020 <133782007+Mhmdmkbl2020@users.noreply.github.com>
2025-04-27 23:08:21 +0300

    Create AppDatabase.kt

---
+++ MobileApp/app/src/main/java/com/example/mysyncapp/database/AppDatabase.kt
@@ -0,0 +1,22 @@
+// MobileApp/app/src/main/java/com/example/mysyncapp/database/AppDatabase.kt
+@Database(entities = [Product::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun productDao(): ProductDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
